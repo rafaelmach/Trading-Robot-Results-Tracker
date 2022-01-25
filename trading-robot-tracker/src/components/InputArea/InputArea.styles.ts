dd9a7bf93e9ab6cfb527a90e391b032262435b35
Rafael Machado <85365177+rafaelmach@users.noreply.github.com>
2022-01-25 12:34:00 -0300

    Lógica de inclusão de dados

--- trading-robot-tracker/src/components/InputArea/InputArea.styles.ts
+++ trading-robot-tracker/src/components/InputArea/InputArea.styles.ts
@@ -2,8 +2,9 @@ import styled from "styled-components"
 
 export const Container = styled.div`
   background-color: #fff;
-  box-shadow: 0px 0px 5px #ccc;
-  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
+    rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+  border-radius: 3px;
   padding: 20px;
   margin-top: 20px;
   display: flex;
@@ -12,10 +13,13 @@ export const Container = styled.div`
 export const InputLabel = styled.label`
   flex: 1;
   margin: 10px;
+  font-family: "Montserrat", sans-serif;
+  font-weight: 500;
 `
 export const InputTitle = styled.div`
   font-weight: bold;
   margin-bottom: 5px;
+  color: #000;
 `
 export const Input = styled.input`
   width: 100%;
@@ -23,6 +27,8 @@ export const Input = styled.input`
   padding: 0 5px;
   border: 1px solid lightblue;
   border-radius: 5px;
+  font-family: "Montserrat", sans-serif;
+  font-weight: 500;
 `
 export const Select = styled.select`
   width: 100%;
@@ -30,19 +36,25 @@ export const Select = styled.select`
   padding: 0 5px;
   border: 1px solid lightblue;
   border-radius: 5px;
+  font-family: "Montserrat", sans-serif;
+  
+  option {
+    font-weight: 500;
+  }
 `
 export const Button = styled.button`
   width: 100%;
   height: 30px;
   padding: 0 5px;
-  border: 1px solid lightblue;
+  border: none;
   border-radius: 5px;
-  background-color: lightblue;
-  color: black;
+  background-color: #05386b;
+  color: #fff;
+  font-weight: 700;
   cursor: pointer;
 
   &:hover {
-    background-color: blue;
+    background-color: #000;
     color: white;
   }
 `
